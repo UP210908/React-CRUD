9dd4d4203c95be7f1ca27784a38195f3b4db3821
UP210908 <up210908@alumnos.upa.edu.mx>
2024-08-25 20:44:16 -0600

    Parte final

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.post("/create", (req, res)=>{
             if(err){
                 console.log(err);
             }else{
-                res.send("Empleado registrado con exito")
+                res.send(result)
             }
         }
     )
@@ -57,7 +57,20 @@ app.put("/update", (req, res)=>{
             if(err){
                 console.log(err);
             }else{
-                res.send("Empleado actualizado con exito")
+                res.send(result)
+            }
+        }
+    )
+});
+
+app.delete("/delete/:id", (req, res)=>{
+    const id = req.params.id;
+    db.query('DELETE from dt_empledos.tr_empleados where id=?', id,
+        (err, result)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.send(result)
             }
         }
     )
